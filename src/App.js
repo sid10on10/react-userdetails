a0e3985232648753bca2b32d65ef994caeb2a6da
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-10-16 11:14:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header"
+import Navbar from "./Components/Navbar"
+import UserForm from "./Components/UserForm"
+import Table from "./Components/Table"
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom' 
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Router>
+        <Header />
+        <Navbar />
+        <Switch>
+          <Route path = "/users" component = {Table} />
+          <Route path = "/" component = {UserForm} />
+        </Switch>
+     </Router>
     </div>
+   
   );
 }
 
